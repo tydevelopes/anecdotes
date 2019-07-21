ccfc62b2a450ad2f2c2a58065f701f5ee9c03d58
Emmanuel <eadjeifrimpong@worcester.edu>
2019-07-21 16:08:55 -0400

    step-1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = props => {
+  const [selected, setSelected] = useState(0);
+
+  const handleClick = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    console.log(randomIndex);
+
+    setSelected(randomIndex);
+  };
+
+  return (
+    <div>
+      <p>{props.anecdotes[selected]}</p>
+      <Button onClick={handleClick} text="next anecdote" />
+    </div>
+  );
+};
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+];
+
+ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'));
