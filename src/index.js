83a2acf2e196e7429f799d197477dd7cc164a047
Emmanuel <eadjeifrimpong@worcester.edu>
2019-07-21 21:12:13 -0400

    add most voted anecdotes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
-const App = props => {
+const Title = ({ text, level }) => {
+  switch (level) {
+    case 1:
+      return <h1>{text}</h1>;
+    case 2:
+      return <h2>{text}</h2>;
+    case 3:
+      return <h3>{text}</h3>;
+    case 4:
+      return <h4>{text}</h4>;
+    case 5:
+      return <h5>{text}</h5>;
+    case 6:
+      return <h6>{text}</h6>;
+    default:
+      return <h1>{text}</h1>;
+  }
+};
+
+const DisplayAnecdote = ({ anecdote, votes }) => {
+  return (
+    <>
+      <p>{anecdote}</p>
+      <p>has {votes} votes</p>
+    </>
+  );
+};
+
+const App = ({ anecdotes, initialPoints }) => {
   const [selected, setSelected] = useState(0);
   const [points, setPoints] = useState(initialPoints);
+  const [maxIndex, setMaxIndex] = useState(0);
 
   const handleNewAnecdote = () => {
     const randomIndex = Math.floor(Math.random() * anecdotes.length);
@@ -16,14 +45,33 @@ const App = props => {
   const handleVote = () => {
     setPoints({ ...points, [selected]: points[selected] + 1 });
   };
-  console.log(points);
+
+  useEffect(() => {
+    let maxIndex = 0;
+    let max = points[0];
+    for (const key in points) {
+      if (points[key] > max) {
+        max = points[key];
+        maxIndex = key;
+      }
+    }
+    setMaxIndex(maxIndex);
+  }, [selected, points]);
 
   return (
     <div>
-      <p>{props.anecdotes[selected]}</p>
-      <p>has {points[selected]} votes</p>
+      <Title text="Anecdote of the day" />
+      <DisplayAnecdote
+        anecdote={anecdotes[selected]}
+        votes={points[selected]}
+      />
       <Button onClick={handleVote} text="vote" />
       <Button onClick={handleNewAnecdote} text="next anecdote" />
+      <Title text="Anecdote with the most votes" level={2} />
+      <DisplayAnecdote
+        anecdote={anecdotes[maxIndex]}
+        votes={points[maxIndex]}
+      />
     </div>
   );
 };
@@ -42,4 +90,9 @@ for (const key of anecdotes.keys()) {
   initialPoints[key] = 0;
 }
 
-ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'));
+ReactDOM.render(
+  <App anecdotes={anecdotes} initialPoints={initialPoints} />,
+  document.getElementById('root')
+);
+
+// TODO: render all anecdotes with the most votes
